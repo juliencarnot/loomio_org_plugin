6b7a9bd31886ac6463f4641f334713953faebf88
Robert Guthrie <rguthrie@gmail.com>
2017-07-19 14:13:53 +1200

    add your groups and plan

--- app/services/chargify_service.rb
+++ app/services/chargify_service.rb
@@ -2,16 +2,25 @@ class ChargifyService
   STANDARD_PLAN_URL = "http://#{ENV['CHARGIFY_APP_NAME']}.chargify.com/subscribe/#{ENV['CHARGIFY_STANDARD_PLAN_KEY']}/#{ENV['CHARGIFY_STANDARD_PLAN_NAME']}"
   PLUS_PLAN_URL = "http://#{ENV['CHARGIFY_APP_NAME']}.chargify.com/subscribe/#{ENV['CHARGIFY_PLUS_PLAN_KEY']}/#{ENV['CHARGIFY_PLUS_PLAN_NAME']}"
 
+  def self.params_for(group, user = nil)
+    h = {organization: group.name,
+         reference: group.key}
+    if user
+      h.merge({first_name: user.first_name, last_name: user.last_name, email: user.email})
+    end
+    params_of(h)
+  end
+
   def self.params_of(hash)
     "?"+hash.map{|k, v| "#{k}=#{CGI.escape(v)}"}.join('&')
   end
 
-  def self.standard_plan_url(group)
-    STANDARD_PLAN_URL + params_of(organization: group.name, reference: group.key)
+  def self.standard_plan_url(group, user = nil)
+    STANDARD_PLAN_URL + params_for(group, user)
   end
 
-  def self.plus_plan_url(group)
-    PLUS_PLAN_URL + params_of(organization: group.name, reference: group.key)
+  def self.plus_plan_url(group, user = nil)
+    PLUS_PLAN_URL + params_for(group, user)
   end
 
   def initialize(subscription_id)
